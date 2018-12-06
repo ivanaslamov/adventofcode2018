6c1667e3252bc86c0da572096021d1db2e38ac6f
Ivan Aslamov <iaslamov@hackerone.com>
2018-12-05 17:15:37 -0800

    added solved check

--- Day05/question1/Main.hs
+++ Day05/question1/Main.hs
@@ -6,38 +6,38 @@ import Data.Maybe
 import Control.Applicative
 import qualified Data.Map.Strict as Map
 
-data Tree = Node Char Char Int Tree Tree | Leaf Char | Empty deriving (Show)
+data Tree = Node Bool Char Char Int Tree Tree | Leaf Char | Empty deriving (Show)
 
 instance Eq Tree where
-    (Leaf c)         == (Leaf c')           = c == c'
-    (Node a b c _ _) == (Node a' b' c' _ _) = (a,b,c) == (a',b',c')
-    _                == _                   = False
+    (Leaf c)           == (Leaf c')             = c == c'
+    (Node _ a b c _ _) == (Node _ a' b' c' _ _) = (a,b,c) == (a',b',c')
+    _                  == _                     = False
 
 firstCharacter :: Tree -> Char
-firstCharacter (Node c _ _ _ _) = c
-firstCharacter (Leaf c)         = c
+firstCharacter (Node _ c _ _ _ _) = c
+firstCharacter (Leaf c)           = c
 
 lastCharacter :: Tree -> Char
-lastCharacter (Node _ c _ _ _) = c
-lastCharacter (Leaf c)         = c
+lastCharacter (Node _ _ c _ _ _) = c
+lastCharacter (Leaf c)           = c
 
 treeLength :: Tree -> Int
-treeLength (Leaf _)           = 1
-treeLength (Node _ _ len _ _) = len
+treeLength (Leaf _)             = 1
+treeLength (Node _ _ _ len _ _) = len
 
 trimFirstCharacter :: Tree -> Maybe Tree
-trimFirstCharacter (Leaf _)                                         = Nothing
-trimFirstCharacter (Node _ _              _   (Leaf _)  right_tree) = Just right_tree
-trimFirstCharacter (Node _ last_character len left_tree right_tree) = Just $ Node first_character' last_character len' left_tree' right_tree
+trimFirstCharacter (Leaf _)                                                = Nothing
+trimFirstCharacter (Node _      _ _              _   (Leaf _)  right_tree) = Just right_tree
+trimFirstCharacter (Node solved _ last_character len left_tree right_tree) = Just $ Node solved first_character' last_character len' left_tree' right_tree
   where
     (Just left_tree') = trimFirstCharacter left_tree
     first_character'  = firstCharacter left_tree'
     len'              = len - 1
 
 trimLastCharacter :: Tree -> Maybe Tree
-trimLastCharacter (Leaf _)                                          = Nothing
-trimLastCharacter (Node _               _ _   left_tree (Leaf _))   = Just left_tree
-trimLastCharacter (Node first_character _ len left_tree right_tree) = Just $ Node first_character last_character' len' left_tree right_tree'
+trimLastCharacter (Leaf _)                                                 = Nothing
+trimLastCharacter (Node _      _               _ _   left_tree (Leaf _))   = Just left_tree
+trimLastCharacter (Node solved first_character _ len left_tree right_tree) = Just $ Node solved first_character last_character' len' left_tree right_tree'
   where
     (Just right_tree') = trimLastCharacter right_tree
     last_character'    = lastCharacter right_tree'
@@ -50,7 +50,7 @@ trim = f . f
 --
 expand :: [Char] -> Tree
 expand [c] = Leaf c
-expand cs  = Node first_character last_character len left_tree right_tree
+expand cs  = Node False first_character last_character len left_tree right_tree
   where
     len = length cs
     middle_position = len `div` 2
@@ -66,11 +66,12 @@ expand cs  = Node first_character last_character len left_tree right_tree
 --
 solution :: Tree -> Maybe Tree
 solution (Leaf c) = Just (Leaf c)
-solution (Node start_character end_character len left_tree right_tree)
+solution (Node True start_character end_character len left_tree right_tree) = Just $ Node True start_character end_character len left_tree right_tree
+solution (Node _ start_character end_character len left_tree right_tree)
     | converges && (isNothing maybe_left_tree) && (isNothing maybe_right_tree) = Nothing
     | converges && (isNothing maybe_left_tree)                                 = solution right_tree'
     | converges                                && (isNothing maybe_right_tree) = solution left_tree'
-    | converges                                                                = solution $ Node start_character' end_character' len' left_tree' right_tree'
+    | converges                                                                = solution $ Node False start_character' end_character' len' left_tree' right_tree'
   where
     lastLeftCharacter   = lastCharacter left_tree
     firstRightCharacter = firstCharacter right_tree
@@ -87,12 +88,12 @@ solution (Node start_character end_character len left_tree right_tree)
     end_character'   = (lastCharacter right_tree')
 
     len' = (treeLength left_tree') + (treeLength right_tree')
-solution (Node start_character end_character len left_tree right_tree)
+solution (Node _ start_character end_character len left_tree right_tree)
     | (isNothing maybe_left_tree) && (isNothing maybe_right_tree) = Nothing
     | (isNothing maybe_left_tree)                                 = maybe_right_tree
     |                                (isNothing maybe_right_tree) = maybe_left_tree
-    | left_tree' == left_tree     && right_tree' == right_tree    = Just $ Node start_character end_character len left_tree right_tree
-    | otherwise                                                   = solution $ Node start_character' end_character' len' left_tree' right_tree'
+    | left_tree' == left_tree     && right_tree' == right_tree    = Just $ Node True start_character end_character len left_tree right_tree
+    | otherwise                                                   = solution $ Node False start_character' end_character' len' left_tree' right_tree'
   where
     maybe_left_tree  = solution left_tree
     maybe_right_tree = solution right_tree
@@ -108,9 +109,9 @@ solution (Node start_character end_character len left_tree right_tree)
 
 --
 pack :: Maybe Tree -> [Char]
-pack Nothing                        = ""
-pack (Just (Leaf c))                = [c]
-pack (Just (Node _ _ _ right left)) = (pack $ Just right) ++ (pack $ Just left)
+pack Nothing                          = ""
+pack (Just (Leaf c))                  = [c]
+pack (Just (Node _ _ _ _ right left)) = (pack $ Just right) ++ (pack $ Just left)
 
 
 --
